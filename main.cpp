59784b2524c28c66fb0697a900997ce469c39fd5
Nicolas Moinard <rekwasss@gmail.com>
2022-12-08 14:51:48 +0100

    feat(check arguments): Check the argument validity in order to launch a specif day

--- main.cpp
+++ main.cpp
@@ -2,19 +2,33 @@
 
 int calorieCounting();
 
-bool displayHelp(const int ac, const char* av[])
+bool displayHelp(const int ac, const char* const av[])
 {
     bool helpDisplayed = false;
-    if (ac == 2 and (strcmp(av[1], "-h") == 0 or (strcmp(av[1], "--help") == 0))) {
+    if (ac == 1 or (ac == 2 and (strcmp(av[1], "-h") == 0 or (strcmp(av[1], "--help") == 0)))) {
         std::cout << "À L'AIDE" << std::endl;
         helpDisplayed = true;
     }
     return helpDisplayed;
 }
 
+bool checkArgs(const int ac, const char* const av[])
+{
+    if (ac != 2) {
+        std::cerr << "Invalid number of arguments" << std::endl;
+        return false;
+    }
+    const int selectedDay = std::atoi(av[1]);
+    if (strcmp(av[1], "0") != 0 and selectedDay <= 0 or selectedDay > 25) {
+        std::cerr << "Invalid argument, you must enter a number between 0 and 25" << std::endl;
+        return false;
+    }
+    return true;
+}
+
 int main(const int ac, const char* av[])
 {
-    if (displayHelp(ac, av)) {
+    if (displayHelp(ac, av) or not checkArgs(ac, av)) {
         return 1;
     }
     calorieCounting();
