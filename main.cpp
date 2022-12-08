934125e795dd6dd216100653e5f1e2cdb9fddad1
Nicolas Moinard <rekwasss@gmail.com>
2022-12-08 13:02:26 +0100

    feat(help flag): Added the -h or --help flag in order to display how to use the programm

--- main.cpp
+++ main.cpp
@@ -2,9 +2,21 @@
 
 int calorieCounting();
 
-int main()
+bool displayHelp(const int ac, const char* av[])
 {
+    bool helpDisplayed = false;
+    if (ac == 2 and (strcmp(av[1], "-h") == 0 or (strcmp(av[1], "--help") == 0))) {
+        std::cout << "À L'AIDE" << std::endl;
+        helpDisplayed = true;
+    }
+    return helpDisplayed;
+}
 
+int main(const int ac, const char* av[])
+{
+    if (displayHelp(ac, av)) {
+        return 1;
+    }
     calorieCounting();
     return 0;
 }
