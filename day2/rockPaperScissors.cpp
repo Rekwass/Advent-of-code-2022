f38f5619f19eccf694731ec155a793080158048f
Nicolas Moinard <rekwasss@gmail.com>
2023-03-16 23:30:33 +0100

    feat(day2): in C++

--- day2/rockPaperScissors.cpp
+++ day2/rockPaperScissors.cpp
@@ -1,7 +1,58 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 
-void rockPaperScissors()
+// A == X == ROCK == 1
+// B == Y == PAPER == 2
+// C == Z == SCISSORS == 3
+//
+// X == LOSS
+// Y == DRAW
+// Z == WIN
+//
+// LOSS = 0
+// DRAW = 3
+// WIN = 6
+
+// ROCK PAPER
+// PAPER SCISSORS
+// SCISSORS ROCK
+
+int getPlayValue(const char c)
 {
-    std::cout << "Not implemented." << std::endl;
+    if (c == 'X') {
+        return 1;
+    }
+    if (c == 'Y') {
+        return 2;
+    }
+    return 3;
+}
+
+int matchRPS(const char us, const char enemy)
+{
+    if ((us == 'X' and enemy == 'B') or (us == 'Y' and enemy == 'C') or (us == 'Z' and enemy == 'A')) {
+        return 0;
+    }
+    if ((us == 'X' and enemy == 'A') or (us == 'Y' and enemy == 'B') or (us == 'Z' and enemy == 'C')) {
+        return 3;
+    }
+    return 6;
+}
+
+int calculateScore(const char us, const char enemy)
+{
+    return (getPlayValue(us) + matchRPS(us, enemy));
+}
+
+void rockPaperScissors(std::ifstream& fileContent)
+{
+    char us = 0;
+    char enemy = 0;
+    int result = 0;
+
+    while (fileContent >> enemy >> us) {
+        result += calculateScore(us, enemy);
+    }
+    std::cout << "Score: " << result << std::endl;
 }
